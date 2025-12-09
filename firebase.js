56f059a83e7a10a44122a8fee00977616619967c
dadakadam843-star <dadakadam843@gmail.com>
2025-12-09 08:47:21 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,22 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+<!-- firebase.js -->
+<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js"></script>
 
+<script>
 const firebaseConfig = {
   apiKey: "AIzaSyDW9Au99PFKyMz7kBKypIKPIOex_ZkJ3uY",
   authDomain: "public-working.firebaseapp.com",
   projectId: "public-working",
-  storageBucket: "public-working.firebasestorage.app",
+  storageBucket: "public-working.appspot.com",
   messagingSenderId: "266169229854",
   appId: "1:266169229854:web:87218d1d726fb50714840f"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+</script>
